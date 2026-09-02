3a4b6515102816d6652b15acc0089185bf61bcb4
Dua.07 <desktop.home321@gmail.com>
2026-09-02 16:52:06 +0500

    first commit

---
+++ randomfile.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r'C:\Users\Zone Tech\Downloads\students.csv')
+
+number = df['city'].value_counts()
+
+plt.bar(number.index, number.values, color = "teal")
+plt.xlabel("Cites")
+plt.ylabel("Students")
+plt.title("Number of Students per city")
+plt.show()
